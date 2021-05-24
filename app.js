11ec3c55c0fd562b92f3ca52719fd1a30aaba220
Jared Irwin <jaredtirwin@gmail.com>
2021-05-24 17:30:36 -0400

    Finished

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const {
   models: { User, Note },
-} = require('./db');
-const path = require('path');
+} = require("./db");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+const requireToken = async (req,res,next) => {
+  try {
+  const token = req.headers.authorization
+  const user = await User.byToken(token)
+  req.user = user
+  next();
+  } catch(error) {
+    next(error);
+  }
+}
 
-app.post('/api/auth', async (req, res, next) => {
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.post("/api/auth", async (req, res, next) => {
   try {
     res.send({ token: await User.authenticate(req.body) });
   } catch (ex) {
@@ -16,21 +28,25 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user)
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/users/:id/notes', async (req, res, next) => {
-  const userId = req.params.id;
-  const notes = await Note.findAll({
-    where: { userId: req.params.id },
-    raw: true,
-  });
-  res.json(notes);
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
+  const userId = +req.params.id;
+  if (
+    userId === +req.user.id 
+  ) {
+    const notes = await Note.findAll({
+      where: { userId: req.params.id },
+      raw: true,
+    });
+    res.json(notes);
+  }
 });
 
 app.use((err, req, res, next) => {
